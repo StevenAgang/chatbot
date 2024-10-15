fe7245e3d6af22feac0a37b8c265ae3b955a4e6e
Steven Agang <agangsteven19@gmail.com>
2024-10-15 22:09:21 +0800

    updating index.php

--- index.php
+++ index.php
@@ -313,6 +313,4 @@ switch (ENVIRONMENT)
  * And away we go...
  */
 require_once BASEPATH . 'dotenv/autoloader.php';
-$dotenv = new Dotenv\Dotenv(__DIR__);
-$dotenv->load();
 require_once BASEPATH.'core/CodeIgniter.php';
